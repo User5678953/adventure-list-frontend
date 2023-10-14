11fedb9db71e9d533f619555ada0337804f75f96
samphillips123 <sphillips123@gmail.com>
2023-10-14 15:15:48 -0700

    moved PhotosDiv to components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from './Login'
 import MapBoard from '../components/MapBoard'
-import PhotosDiv from './PhotosDiv'
+import PhotosDiv from '../components/PhotosDiv'
 
 const Home = () => {
 
     return (
-        <div>
+        <div className='homePage'>
             <Routes>
                 <Route path='/register/login' element={
                     <Login /> 
