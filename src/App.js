ccf0dbf7fb6f3250ba545fb607f3a25b9f9b66ba
kendynweb <kendynweb@gmail.com>
2023-10-15 12:23:44 -0400

    Branch created

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
+<<<<<<< Updated upstream
 import Home from './pages/Home';
+=======
+import Login from "./pages/Login";
+>>>>>>> Stashed changes
 
 
 function App() {
   return (
     <div className="App">
-      <header >
+      <header>
         <Navbar />
+<<<<<<< Updated upstream
         <Home />
         
+=======
+
+        <h1>THIS IS APP.JS</h1>
+        <Route path="/login" component={Login} />
+>>>>>>> Stashed changes
       </header>
     </div>
   );
