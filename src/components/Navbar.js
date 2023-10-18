37bdcb8bc96cce037c42f84b945304f17bf46b19
Daniel Machin <DanielTAYG@gmail.com>
2023-10-18 18:04:02 -0400

    added cookies i think ?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,65 @@
 // import {Link, Route, Routes} from 'react-router-dom'
 // import Login from '../pages/Login'
 import React from "react"
+import Cookies from "universal-cookie"
+const cookies = new Cookies()
 
 const Navbar = () => {
-    return (
-        <nav className='navbar'>
-            <a href='/' className='site-title'>
-                AdventureList 2.0
-            </a>
-            <ul>
-                <li>
-                    <a href='/login'>Login/Register</a>
-                </li>
-            </ul>
 
-            {/* <Link to='/register/login'>
-                <div>Login</div>
-            </Link> */}
-               {/* <Routes>
-                <Route path='/register/login' element={
-                    <Login />
-                } 
+    const token = cookies.get("TOKEN")
 
-                />
-            </Routes> */}
-        </nav>
+    const notLoggedIn = () => {
+        return (
+            
+                <>
+          
+                <nav className='navbar'>
+                    <a href='/' className='site-title'>
+                        AdventureList 2.0
+                    </a>
+                    <ul>
+                        <li>
+                            <a href='/login'>Login/Register</a>
+                        </li>
+                    </ul>
+        
+                </nav>
+        
+                </> 
+            
+        )
+    }
+
+    const loggedIn = () => {
+        return (
+            
+            <>
+      
+            <nav className='navbar'>
+                <a href='/' className='site-title'>
+                    AdventureList 2.0
+                </a>
+                <ul>
+                    <li>
+                        <a href='/login'>New Adventure</a>
+                    </li>
+                    <li>
+                        <a href='/login'>Logout</a>
+                    </li>
+                </ul>
+    
+            </nav>
+    
+            </> 
+        
     )
+    }
+
+    
+    return (
+        token ? loggedIn() : notLoggedIn()
+    )
+
 }
 
 export default Navbar
