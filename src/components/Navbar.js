94f30742d0a981e82a66cdb812520b51717ed455
Daniel Machin <DanielTAYG@gmail.com>
2023-10-18 19:21:11 -0400

    Revert "added cookies i think ?"
    This reverts commit 37bdcb8bc96cce037c42f84b945304f17bf46b19.
    
    Fixing Merge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,65 +1,31 @@
 // import {Link, Route, Routes} from 'react-router-dom'
 // import Login from '../pages/Login'
 import React from "react"
-import Cookies from "universal-cookie"
-const cookies = new Cookies()
 
 const Navbar = () => {
-
-    const token = cookies.get("TOKEN")
-
-    const notLoggedIn = () => {
-        return (
-            
-                <>
-          
-                <nav className='navbar'>
-                    <a href='/' className='site-title'>
-                        AdventureList 2.0
-                    </a>
-                    <ul>
-                        <li>
-                            <a href='/login'>Login/Register</a>
-                        </li>
-                    </ul>
-        
-                </nav>
-        
-                </> 
-            
-        )
-    }
-
-    const loggedIn = () => {
-        return (
-            
-            <>
-      
-            <nav className='navbar'>
-                <a href='/' className='site-title'>
-                    AdventureList 2.0
-                </a>
-                <ul>
-                    <li>
-                        <a href='/login'>New Adventure</a>
-                    </li>
-                    <li>
-                        <a href='/login'>Logout</a>
-                    </li>
-                </ul>
-    
-            </nav>
-    
-            </> 
-        
-    )
-    }
-
-    
     return (
-        token ? loggedIn() : notLoggedIn()
-    )
+        <nav className='navbar'>
+            <a href='/' className='site-title'>
+                AdventureList 2.0
+            </a>
+            <ul>
+                <li>
+                    <a href='/login'>Login/Register</a>
+                </li>
+            </ul>
 
+            {/* <Link to='/register/login'>
+                <div>Login</div>
+            </Link> */}
+               {/* <Routes>
+                <Route path='/register/login' element={
+                    <Login />
+                } 
+
+                />
+            </Routes> */}
+        </nav>
+    )
 }
 
 export default Navbar
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,8 +2,6 @@ import axios from "axios"
 import { useState } from "react"
 import React from "react"
 import { Link, useNavigate } from "react-router-dom"
-import Cookies from "universal-cookie"
-const cookies = new Cookies()
 
 
 const Login = () => {
@@ -21,7 +19,6 @@ const Login = () => {
                 username, password
             } ).then(res =>{
                 if(res.data==="logged in") {
-                    cookies.set("TOKEN", res.data.token, {path: '/',})
                     move('/')
                     window.location.reload()
                 } else {
