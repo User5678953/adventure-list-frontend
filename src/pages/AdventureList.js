135a0970e8fa968837afcc29491bb86c769b8ce6
Daniel Machin <DanielTAYG@gmail.com>
2023-10-21 18:03:09 -0400

    added logging out succesfully

--- src/pages/AdventureList.js
+++ src/pages/AdventureList.js
@@ -1,3 +1,10 @@
+import axios from "axios"
+import { useState } from "react"
+import React from "react"
+import { Link, useNavigate } from "react-router-dom"
+import Cookies from "universal-cookie"
+const cookies = new Cookies()
+
 const AdventureList = () => {
     return (
         <h1>Adventure List Page</h1>
