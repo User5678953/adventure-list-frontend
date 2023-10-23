3641ce3e79a490841842d9d75e666dd7695f850b
kendynweb <kendynweb@gmail.com>
2023-10-23 13:37:57 -0400

    Fixed login/register styling

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,51 +1,68 @@
-import axios from "axios"
-import { useState } from "react"
-import React from "react"
-import { useNavigate } from "react-router-dom"
-import Cookies from "universal-cookie"
-const cookies = new Cookies()
+import axios from "axios";
+import { useState } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
+  const move = useNavigate();
 
-    const move = useNavigate()
+  async function submit(e) {
+    e.preventDefault();
 
-
-    async function submit(e) {
-        e.preventDefault()
-
-        try {
-            await axios.post("http://localhost:3000/register", {
-                username, password
-            } ).then(res => {
-                if(res.data==="Success"){
-                    cookies.set("TOKEN", res.data.token, {path: '/',})
-                    move('/')
-                    window.location.reload()
-                } else {
-                    alert("try again")
-                }
-            })
-             console.log("User Registered")
-        } catch (err) {
-            console.log(err)
-        }
+    try {
+      await axios
+        .post("http://localhost:3000/register", {
+          username,
+          password,
+        })
+        .then((res) => {
+          if (res.data === "Success") {
+            cookies.set("TOKEN", res.data.token, { path: "/" });
+            move("/");
+            window.location.reload();
+          } else {
+            alert("try again");
+          }
+        });
+      console.log("User Registered");
+    } catch (err) {
+      console.log(err);
     }
+  }
 
-    return (
-        <div className="login-container">
-            <h2>Register</h2>
-        
-            <form action="POST">
-                <input type="username" onChange={(e) => {setUsername(e.target.value)}} placeholder="Username"></input>
-                <input type="password" onChange={(e) => {setPassword(e.target.value)}} placeholder="Password"></input>
-                <input className="submit-btn" type="submit" onClick={submit}></input>
-            </form>
-        
-        </div>
-    )
-}
+  return (
+    <div className="form-fill-modal">
+      <form action="POST" className="form-background">
+        <h1>Register</h1>
+        <label>Username:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+          required
+        />
 
-export default Register
+        <label>Password:</label>
+        <input
+          type="text"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+          required
+        />
+
+        <button type="submit" onClick={submit} className="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
