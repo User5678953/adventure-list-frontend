070a1e729bb43e95f74ad9c6bb8bf096f24c2ad1
samphillips123 <sphillips123@gmail.com>
2023-10-23 15:07:37 -0700

    installed react-multi-carousel package and set up in AdventureCarousel.js

--- src/components/AdventureCarousel.js
+++ src/components/AdventureCarousel.js
@@ -4,6 +4,10 @@ import { useEffect, useState } from "react";
 import PhotoUploadForm from "./AddPhotosForm";
 import Modal from "./Modal";
 
+// IMPORT REACT-MULTI-CAROUSEL NPM PACKAGE
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css"
+
 // IMPORT TEST DATA
 import advTestData from './adventureCards/TestAdvData'
 
@@ -29,42 +33,101 @@ const AdventureCarousel = () => {
   const [adventure, setAdventure] = useState([])
 
   const getAdventure = async () => {
-    const response = await fetch (`${backendURL}/adventureList`)
+    const response = await fetch(`${backendURL}/adventureList`)
     const data = await response.json()
     setAdventure(data)
   }
 
   useEffect(() => {
     getAdventure()
-  },[])
+  }, [])
 
   console.log(adventure)
-  
-    return (
-      <>
-        <div className="adventure-carousel">
-            {/* <h1>Adventure Carousel Component</h1> */}
-            <div className="adventures" style={{width: "18rem"}}>
-                {adventure.map((adventure, i) => {
-                    return (<AdvCard {...adventure} key={i} />)
-                })}
-            </div>
-        </div>
+
+  return (
+    <>
+      <div className="adventure-carousel">
+        {/* <h1>Adventure Carousel Component</h1> */}
+        <Carousel
+          additionalTransfrom={0}
+          arrows
+          autoPlaySpeed={3000}
+          centerMode={false}
+          className=""
+          containerClass="container-with-dots"
+          dotListClass=""
+          draggable
+          focusOnSelect={false}
+          infinite
+          itemClass=""
+          keyBoardControl
+          minimumTouchDrag={80}
+          pauseOnHover
+          renderArrowsWhenDisabled={false}
+          renderButtonGroupOutside={false}
+          renderDotsOutside={false}
+          responsive={{
+            desktop: {
+              breakpoint: {
+                max: 3000,
+                min: 1024
+              },
+              items: 3,
+              partialVisibilityGutter: 40
+            },
+            mobile: {
+              breakpoint: {
+                max: 464,
+                min: 0
+              },
+              items: 1,
+              partialVisibilityGutter: 30
+            },
+            tablet: {
+              breakpoint: {
+                max: 1024,
+                min: 464
+              },
+              items: 2,
+              partialVisibilityGutter: 30
+            }
+          }}
+          rewind={false}
+          rewindWithAnimation={false}
+          rtl={false}
+          shouldResetAutoplay
+          showDots={false}
+          sliderClass=""
+          slidesToSlide={1}
+          swipeable
+        >
+
+          {/* <div className="adventures" style={{ width: "18rem" }}> */}
+          {/* use 'adventure' for live data */}
+          {/* {adventure.map((adventure, i) => {
+              return (<AdvCard {...adventure} key={i} />)
+            })} */}
+
+          {/* use 'advTestData' for local test dataset */}
+          {advTestData.map((adventure, i) => {
+            return (<AdvCard {...adventure} key={i} />)
+          })}
+          {/* </div> */}
+        </Carousel>
+      </div>
 
 
-    <div>
-      <h1>Adventure Carousel Component</h1>
+      <div>
+        {/* Button to trigger photo upload modal */}
+        <button onClick={() => setShowUploadModal(true)}>Upload Photo</button>
 
-      {/* Button to trigger photo upload modal */}
-      <button onClick={() => setShowUploadModal(true)}>Upload Photo</button>
-
-      {/* Conditional rendering of the modal with PhotoUploadForm */}
-      {showUploadModal && (
-        <Modal onClose={() => setShowUploadModal(false)}>
-          <PhotoUploadForm />
-        </Modal>
-      )}
-    </div>
+        {/* Conditional rendering of the modal with PhotoUploadForm */}
+        {showUploadModal && (
+          <Modal onClose={() => setShowUploadModal(false)}>
+            <PhotoUploadForm />
+          </Modal>
+        )}
+      </div>
     </>
   );
 };
--- src/components/adventureCards/AdvCard.js
+++ src/components/adventureCards/AdvCard.js
@@ -6,7 +6,7 @@ const AdvCard = ({title, location, photos}) => {
     return (
         <div className="adventure">
             {/* <h1>Adventure Card Component</h1> */}
-            {/* <AdvCardImg photos={photos} /> */}
+            <AdvCardImg photos={photos} />
             <AdvCardBody title={title} location={location} />
         </div>
     )
