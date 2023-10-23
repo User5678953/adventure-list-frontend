91cef2af34bd5084ddf23812f895350b836f6c1b
samphillips123 <sphillips123@gmail.com>
2023-10-23 15:15:56 -0700

    added comments with links to carousel docs

--- src/components/AdventureCarousel.js
+++ src/components/AdventureCarousel.js
@@ -5,6 +5,9 @@ import PhotoUploadForm from "./AddPhotosForm";
 import Modal from "./Modal";
 
 // IMPORT REACT-MULTI-CAROUSEL NPM PACKAGE
+// Documentation: https://react-multi-carousel.surge.sh/?selectedKind=Carousel&selectedStory=With%20infinite%20mode&full=0&addons=1&stories=1&panelRight=0&addonPanel=kadira%2Fjsx%2Fpanel
+// NPM: https://www.npmjs.com/package/react-multi-carousel
+
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css"
 
