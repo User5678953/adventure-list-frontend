eec6e93d311d0a1fd473f24a198e57ec9d83c754
samphillips123 <sphillips123@gmail.com>
2023-10-24 15:25:19 -0700

    added _id to test data for adventures and photos

--- src/components/adventureCards/TestAdvData.js
+++ src/components/adventureCards/TestAdvData.js
@@ -1,17 +1,21 @@
 export default [
     {
+        _id: '1357',
         title: 'Costa Rica 2023',
         description: '5 year anniversary trip.',
         photos: [
             {
+                _id: 'abcd',
                 imageURL: 'https://images.unsplash.com/photo-1580495024618-3fb7d34affbd?auto=format&fit=crop&q=80&w=1171&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'View from Monteverde.'
             },
             {
+                _id: 'efgh',
                 imageURL: 'https://images.unsplash.com/photo-1552727131-5fc6af16796d?auto=format&fit=crop&q=80&w=1049&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'Keel-billed Toucan.'
             },
             {
+                _id: 'ijkl',
                 imageURL: 'https://images.unsplash.com/photo-1626060451577-8b90a394e355?auto=format&fit=crop&q=80&w=1074&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'Playa Minas, Guanacaste, Costa Rica..'
             }
@@ -20,18 +24,22 @@ export default [
         owner: 'Sam'
     },
     {
+        _id: '2468',
         title: 'Wine Tasting in Napa Valley',
         description: 'Time to relax and pretend to be boujee.',
         photos: [
             {
+                _id: 'mnop',
                 imageURL: 'https://images.unsplash.com/photo-1594354713050-b9589421f5ec?auto=format&fit=crop&q=80&w=1171&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'Vinyards at the Castello di Amorosa Winery.'
             },
             {
+                _id: 'qrst',
                 imageURL: 'https://images.unsplash.com/photo-1515458861052-83459d8c949e?auto=format&fit=crop&q=80&w=1032&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'Taking a break from wine tasting.'
             },
             {
+                _id: 'uvwx',
                 imageURL: 'https://images.unsplash.com/photo-1572913017567-02f0649bc4fd?auto=format&fit=crop&q=80&w=1074&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'Wine cellar tours.'
             }
@@ -40,18 +48,22 @@ export default [
         owner: 'Sam'
     },
     {
+        _id: '1458',
         title: 'Corona Capital Music Festival',
         description: 'Why are there no bands singing in Spanish?',
         photos: [
             {
+                _id: 'yzab',
                 imageURL: 'https://qromag.com/wp-content/uploads/2022/12/coronacapital22b.jpg',
                 description: 'Large illuminated alebrijes sculpture at the festival.'
             },
             {
+                _id: 'cdef',
                 imageURL: 'https://images.unsplash.com/photo-1601652589234-c282487ec97e?auto=format&fit=crop&q=80&w=1156&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'Break from the music, touring the city.'
             },
             {
+                _id: 'ghij',
                 imageURL: 'https://images.unsplash.com/photo-1585464231875-d9ef1f5ad396?auto=format&fit=crop&q=80&w=1170&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'More Mexico City.'
             }
