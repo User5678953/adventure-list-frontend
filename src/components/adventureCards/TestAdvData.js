f3dacc69cabbb47753c1d67c34b22d38e702d8e5
samphillips123 <sphillips123@gmail.com>
2023-10-24 11:27:15 -0700

    updated test data to have multiple images:

--- src/components/adventureCards/TestAdvData.js
+++ src/components/adventureCards/TestAdvData.js
@@ -6,6 +6,14 @@ export default [
             {
                 imageURL: 'https://images.unsplash.com/photo-1580495024618-3fb7d34affbd?auto=format&fit=crop&q=80&w=1171&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'View from Monteverde.'
+            },
+            {
+                imageURL: 'https://images.unsplash.com/photo-1552727131-5fc6af16796d?auto=format&fit=crop&q=80&w=1049&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                description: 'Keel-billed Toucan.'
+            },
+            {
+                imageURL: 'https://images.unsplash.com/photo-1626060451577-8b90a394e355?auto=format&fit=crop&q=80&w=1074&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                description: 'Playa Minas, Guanacaste, Costa Rica..'
             }
         ],
         location: 'Costa Rica',
@@ -18,7 +26,16 @@ export default [
             {
                 imageURL: 'https://images.unsplash.com/photo-1594354713050-b9589421f5ec?auto=format&fit=crop&q=80&w=1171&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 description: 'Vinyards at the Castello di Amorosa Winery.'
-            }],
+            },
+            {
+                imageURL: 'https://images.unsplash.com/photo-1515458861052-83459d8c949e?auto=format&fit=crop&q=80&w=1032&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                description: 'Taking a break from wine tasting.'
+            },
+            {
+                imageURL: 'https://images.unsplash.com/photo-1572913017567-02f0649bc4fd?auto=format&fit=crop&q=80&w=1074&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                description: 'Wine cellar tours.'
+            }
+        ],
         location: 'Calistoga, CA',
         owner: 'Sam'
     },
@@ -29,7 +46,16 @@ export default [
             {
                 imageURL: 'https://qromag.com/wp-content/uploads/2022/12/coronacapital22b.jpg',
                 description: 'Large illuminated alebrijes sculpture at the festival.'
-            }],
+            },
+            {
+                imageURL: 'https://images.unsplash.com/photo-1601652589234-c282487ec97e?auto=format&fit=crop&q=80&w=1156&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                description: 'Break from the music, touring the city.'
+            },
+            {
+                imageURL: 'https://images.unsplash.com/photo-1585464231875-d9ef1f5ad396?auto=format&fit=crop&q=80&w=1170&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+                description: 'More Mexico City.'
+            }
+        ],
         location: 'Mexico City',
         owner: 'Sam'
     }
