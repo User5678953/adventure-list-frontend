f96fba0a1e66e81356bcd08881f1d34958669f90
samphillips123 <sphillips123@gmail.com>
2023-10-24 10:48:29 -0700

    set up react-multi-carousel in PhotosCarousel to show 1 photo at a time

--- src/components/PhotosCarousel.js
+++ src/components/PhotosCarousel.js
@@ -17,11 +17,67 @@ const PhotosCarousel = () => {
     return (
         <div className="photo-carousel">
             <h1>Photos Carousel Component</h1>
-            <div className="photos" style={{width: "18rem"}}>
+            <Carousel
+                additionalTransfrom={0}
+                arrows
+                autoPlaySpeed={3000}
+                centerMode={false}
+                className=""
+                containerClass="container-with-dots"
+                dotListClass=""
+                draggable
+                focusOnSelect={false}
+                infinite
+                itemClass=""
+                keyBoardControl
+                minimumTouchDrag={80}
+                pauseOnHover
+                renderArrowsWhenDisabled={false}
+                renderButtonGroupOutside={false}
+                renderDotsOutside={false}
+                responsive={{
+                    desktop: {
+                        breakpoint: {
+                            max: 3000,
+                            min: 1024
+                        },
+                        items: 1,
+                        partialVisibilityGutter: 40
+                    },
+                    mobile: {
+                        breakpoint: {
+                            max: 464,
+                            min: 0
+                        },
+                        items: 1,
+                        partialVisibilityGutter: 30
+                    },
+                    tablet: {
+                        breakpoint: {
+                            max: 1024,
+                            min: 464
+                        },
+                        items: 1,
+                        partialVisibilityGutter: 30
+                    }
+                }}
+                rewind={false}
+                rewindWithAnimation={false}
+                rtl={false}
+                shouldResetAutoplay
+                showDots={false}
+                sliderClass=""
+                slidesToSlide={1}
+                swipeable
+            >
+
+                {/* <div className="photos" style={{width: "18rem"}}> */}
                 {advTestData.map((adventure) => {
                     return (<PhotoCard photos={adventure.photos} />)
                 })}
-            </div>
+                {/* </div> */}
+
+            </Carousel>
         </div>
     )
 }
