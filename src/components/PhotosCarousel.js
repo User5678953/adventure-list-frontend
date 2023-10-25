8a3c83e663177f46da7d5ba1943ff332f425d001
samphillips123 <sphillips123@gmail.com>
2023-10-25 09:07:40 -0700

    cleaned up unused files

--- src/components/PhotosCarousel.js
+++ src/components/PhotosCarousel.js
@@ -1,5 +1,5 @@
 import React from "react"
-import PhotoCardImg from "./photoCards/PhotoCardImg"
+import PhotoCardImg from "./PhotoCardImg"
 
 // IMPORT REACT-MULTI-CAROUSEL NPM PACKAGE
 // Documentation: https://react-multi-carousel.surge.sh/?selectedKind=Carousel&selectedStory=With%20infinite%20mode&full=0&addons=1&stories=1&panelRight=0&addonPanel=kadira%2Fjsx%2Fpanel
