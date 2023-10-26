c6187c4f5268c47ca9fa232ff794639a080a8095
samphillips123 <sphillips123@gmail.com>
2023-10-26 13:52:33 -0700

    verifying edit adventure and upload photo code

--- src/pages/AdventureList.js
+++ src/pages/AdventureList.js
@@ -1,13 +1,12 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
-import Cookies from "universal-cookie";
 import { useEffect, useState } from "react";
-import PhotoUploadForm from "../components/AddPhotosForm";
+import Cookies from "universal-cookie";
+import AddAdventureListForm from "../components/AddAdventureListForm";
 import Modal from "../components/Modal";
+import PhotoUploadForm from "../components/AddPhotosForm";
 
 // Import backend Endpoint
 const backendURL = process.env.REACT_APP_BACKEND_URL;
-
 console.log("Backend URL:", backendURL);
 
 const cookies = new Cookies();
@@ -15,10 +14,9 @@ const cookies = new Cookies();
 const AdventureList = ({ id }) => {
   const [adventure, setAdventure] = useState(null);
   const [showUploadModal, setShowUploadModal] = useState(false);
-
+  const [showEditModal, setShowEditModal] = useState(false);
 
   // fetch request
-
   useEffect(() => {
     if (id) {
       fetch(`${backendURL}/adventureList/${id}`)
@@ -30,10 +28,25 @@ const AdventureList = ({ id }) => {
     }
   }, [id]);
 
+  // Handler to initiate the edit mode
+  const handleEditClick = () => {
+    setShowEditModal(true);
+  };
+
+  // Handler to close the edit mode
+  const handleEditClose = () => {
+    setShowEditModal(false);
+  };
+
+  // Handler to close the edit form
+  const handleCloseFunction = () => {
+    setShowUploadModal(false);
+  };
+
   return (
     <>
       <div className="adventure-list">
-        {adventure ? (
+        {adventure && !showEditModal ? (
           <>
             <h2>{adventure.title}</h2>
             <p>Description: {adventure.description}</p>
@@ -42,8 +55,15 @@ const AdventureList = ({ id }) => {
             <p>Completed: {adventure.completed ? "Yes" : "No"}</p>
             <p>Tags: {adventure.tags}</p>
             <div className="button-container">
-              <button className="advent-button advent-button-left">Edit</button>
-              <button className="advent-button advent-button-delete">Delete</button>
+              <button
+                className="advent-button advent-button-left"
+                onClick={handleEditClick}
+              >
+                Edit
+              </button>
+              <button className="advent-button advent-button-delete">
+                Delete
+              </button>
               <button
                 className="advent-button advent-button-color"
                 onClick={() => setShowUploadModal(true)}
@@ -56,10 +76,20 @@ const AdventureList = ({ id }) => {
           <p>Select an Adventure below or add one! Get Adventuring already!</p>
         )}
 
+        {/* Conditional rendering of the edit modal with AddAdventureListForm */}
+        {showEditModal && (
+          <Modal onClose={handleEditClose}>
+            <AddAdventureListForm
+              adventure={adventure}
+              onClose={handleEditClose}
+            />
+          </Modal>
+        )}
+
         {/* Conditional rendering of the modal with PhotoUploadForm */}
         {showUploadModal && (
-          <Modal onClose={() => setShowUploadModal(false)}>
-            <PhotoUploadForm />
+          <Modal onClose={handleCloseFunction}>
+            <PhotoUploadForm onClose={handleCloseFunction} />
           </Modal>
         )}
       </div>
